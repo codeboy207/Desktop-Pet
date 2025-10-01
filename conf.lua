d77fab90e06ad856c46a55abb12c7fe74d7b4e1c
Jerry <jerry.207@outlook.com>
2025-10-01 16:53:13 +0630

    Initial Start

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+	t.window.width = 50
+	t.window.height = 50
+
+	t.modules.joystick = false
+	t.window.vsync = 1
+	t.window.borderless = true
+end
--- main.lua
+++ main.lua
@@ -1,7 +1,26 @@
 function love.load()
+	x = 0
+	y = 0
 end
 
 function love.update(dt)
+	love.window.setPosition(x, y)
+end
+
+function love.keypressed(key)
+	if key == "escape" then
+		love.event.quit()
+	end
+
+	if key == "right" then
+		x = x + 10
+	elseif key == "left" then
+		x = x - 10
+	elseif key == "down" then
+		y = y + 10
+	elseif key == "up" then
+		y = y - 10
+	end
 end
 
 function love.draw()
